a13cb3d325c88457962b26e67dbdb7f36842e1c7
skshivamkedia <skshivamkedia96@gmail.com>
2021-07-25 21:13:04 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f95e300c-7fd8-46b1-a979-59a63ab990e2';
+export const MODAL_CLOSE_SEC = 2.5;
